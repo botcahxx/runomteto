010fe4952e0083066187327fac5559ba781bdbeb
TioXd <94074076+BOTCAHX@users.noreply.github.com>
2022-06-13 20:52:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,26 +13,26 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read pesan / message
-//global.autorecording = true //status auto merekam ( auto record )
-//global.autoketik = false //status auto mengetik (auto typing)
+global.autorecording = true //status auto merekam ( auto record )
+global.autoketik = true //status auto mengetik (auto typing)
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "BOTCAHX" //namabot kalian
+global.ownername= "Tio" //nama kalian
+global.myweb ="myblogxyz.netlify.com" //bebas asal jan hapus
+global.youtube = "https://www.youtube.com/channel/UCCyEu6Ui3JmTON1P8BQydsA" //bebas asal jan hapus
+global.github = "https://github.com/BOTCAHX" //bebas
+global.email = "tioclkp02@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "6281395861695" // nomor wa kalian
+global.ownernomerr = "+6282221792667" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["6281395861695","6281395861695","6282221792667"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© BOTCAHX' //sticker wm ubah
+global.author = 'dibuat oleh bot' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
