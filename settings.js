152738a978fb49ea343a1640d563d79eb0af30c5
TioXd <94074076+BOTCAHX@users.noreply.github.com>
2022-06-14 02:48:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
+global.autoread = true // auto read pesan / message
 global.autorecording = true //status auto merekam ( auto record )
 global.autoketik = true //status auto mengetik (auto typing)
-//global.available = false //status online (online)
+global.available = true //status online (online)
 
 // Other
 global.botname = "BOTCAHX" //namabot kalian
